108d02421973ee9baeeabcaa8abd2d1cfa29467e
Edel <edelmedina322@gmail.com>
2023-04-01 17:49:53 -0300

    Initial commit

---
+++ src/Assets/journal.js
@@ -0,0 +1,14 @@
+const journal = [
+    {
+        date: "April 1st",
+        description: `OK, so actually I hadn't planned for this thing, I'm not even sure if I'll use but I'm kinda seriously considering it rn
+        Since this is day one I don't really wanna commit too hard to anything. I kinda suck at designing things so I was thinking maybe a layout like the IDV event going on rn would be kinda nice, you know, the one where you pick gifts and guess who's gifting em.
+        Ngl tho since there's also an IDV journal it kinda feels like I'm taking everything from there... I SWEAR I'M NOT AN IDV ADDICT AND I'M TRYNA COME UP WITH SHIT MYSELF BUT IT IS WHAT IT IS
+        Also I have two months so I'm sure there'll be some changes, right? You tell me. Making this gift is kinda hard cause what I'm tryna convey more than anything else is something meaningful, I REALLY DON'T WANNA BE CRUNCHY, I JUST DON'T KNOW HOW TO SAY THIS IN ANY OTHER WAY, but there's things you've done and said to me that really have meant the world.
+        BRO THAT DAY YOU WENT FUCKING ALL CRUNCHY OUT OF NOWHERE AND FUCKING KO'D ME WAS CRAZY.
+        No, but real real real Imma try hard to make this gift what I want it to be, hope you like it cause Imma pour my everything into itttttt.
+        Also yesterday was the day you said we should study cats so we can also eat ourselves out, CRAZYYYYYYYYYYYYY.
+        DAY ONE OVER, SEE YOU TOMORROW`
+    }
+]
+export default Journal
